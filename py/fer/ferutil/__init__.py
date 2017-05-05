ca42deab36b0b4d091e1c2a76ca923cd859d0ce7
Guillaume Xavier Taillon <gx@taillon.co>
2017-05-04 20:13:15 -0400

    Added logger

--- py/fer/ferutil/__init__.py
+++ py/fer/ferutil/__init__.py
@@ -1,5 +1,6 @@
 from dummy import *
 from errors import *
+from logger import *
 from pformat import *
 from strictargs import *
 from typecheck import *
--- py/fer/grammer/__init__.py
+++ py/fer/grammer/__init__.py
@@ -1,14 +1,16 @@
 
 import io
-import logging
 
+from fer.ferutil import *
 from compiler import *
 from grammar import *
 from parser import *
 
+log = logger.get_logger()
+
 def compile_parser(grammar_file, parser_file, parser_name):
   result = None
-  logging.info("Parsing grammar file")
+  log.info("Parsing grammar file")
   with io.open(grammar_file, "rb") as f:
     brf = io.BufferedReader(f)
     r = ParseReader(brf)
@@ -17,16 +19,16 @@ def compile_parser(grammar_file, parser_file, parser_name):
     r.consume_ws()
     eof_result = r.consume_eof()
     if not eof_result:
-      logging.error("Failed to parse grammar file")
+      log.error("Failed to parse grammar file")
       result.put(causes=[eof_result])
     elif not result:
-      logging.error("Failed to parse grammar file")
+      log.error("Failed to parse grammar file")
     else:
-      logging.info("Parsed grammar file")
+      log.info("Parsed grammar file")
       with io.open(parser_file, "wb+") as f:
         bwf = io.BufferedWriter(f)
         gpc = GrammarParserCompiler(bwf, result, parser_name)
         gpc()
         bwf.flush()
-        logging.info("Wrote parser file")
+        log.info("Wrote parser file")
   return (r.stats, result)
