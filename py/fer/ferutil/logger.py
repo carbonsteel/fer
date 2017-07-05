ff5d102112d47960943c1a219d3054afaac78f37
gxtaillon <gx@taillon.co>
2017-07-04 20:20:52 -0400

    Added LazyFormat class.

--- py/fer/ferutil/logger.py
+++ py/fer/ferutil/logger.py
@@ -14,6 +14,14 @@ def get_logger():
   module_name = inspect.getmodule(frm[0]).__name__
   return logbook.Logger(module_name)
 
+class LazyFormat(object):
+  def __init__(self, callback, *args, **kwargs):
+    self.callback = callback
+    self.args = args
+    self.kwargs = kwargs
+  def __str__(self):
+    return self.callback(*self.args, **self.kwargs)
+
 def init(level):
   #log_handler = logbook.FileHandler('application.log', 'w')
   log_handler = logbook.StreamHandler(sys.stdout, level=level)
