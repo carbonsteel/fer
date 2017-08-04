b909ab30250f8588d3d42a2e8def4fcbb6e1ebeb
guillaume-taillon <gxt@localhost.localdomain>
2017-08-04 16:29:12 -0400

    Added savelog since it is missing on centos 7. https://launchpad.net/ubuntu/+source/debianutils/4.2.1ubuntu2

--- py/run.sh
+++ py/run.sh
@@ -2,6 +2,6 @@
 LogFile='application.log'
 LogDir='.'
 LogPath="$LogDir/$LogFile"
-/usr/bin/savelog -l -p -d -C -c 5 "$LogPath"
+./tools/savelog -l -p -d -C -c 5 "$LogPath"
 export INCPATH='tests/basic;src/include'
 time /usr/bin/python3.6 -OO -m fer.compiler basic 1>>"$LogPath" 2>&1
