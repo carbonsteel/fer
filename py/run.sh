7f1491cf7989657a349ebaa2f475760d835e363f
guillaume-taillon <gxt@localhost.localdomain>
2017-08-04 18:47:22 -0400

    Create lang folder for everything language related and not otherwise compiler related.

--- py/run.sh
+++ py/run.sh
@@ -3,5 +3,5 @@ LogFile='application.log'
 LogDir='.'
 LogPath="$LogDir/$LogFile"
 ./tools/savelog -l -p -d -C -c 5 "$LogPath"
-export INCPATH='tests/basic;src/include'
+export INCPATH='lang/tests/basic;lang/include'
 time /usr/bin/python3.6 -OO -m fer.compiler basic 1>>"$LogPath" 2>&1
